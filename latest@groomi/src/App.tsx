84d90fdb42171ce9d7f23a28e1b6d73b94749de1
groomigroom <googjohn0143@gmail.com>
2026-03-07 07:15:53 +0000

    1

--- latest@groomi/src/App.tsx
+++ latest@groomi/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,25 +6,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      <h1>리액트 예제</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          기본 <code>예제</code> 입니다.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        예제는 이렇게 만듭니다.<br></br>
+        npm create vite@latest groomi, npm run dev, npm install
       </p>
     </>
   )
